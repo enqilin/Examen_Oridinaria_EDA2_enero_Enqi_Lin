4bb704f5f6ba40d42bcde9dce2ba5bcba8f8769b
nadous <114181724+enqilin@users.noreply.github.com>
2023-05-25 08:48:10 +0000

    grafo inserta vertice

--- pregunta9.py
+++ pregunta9.py
@@ -14,4 +14,27 @@ class nodoVertice(object):
         self.adyacentes = Arista()
 
 class Grafo(object):
-    
+    def __init__(self,dirigido=True):
+        self.inicio = None
+        self.tamanio = 0
+        self.dirigido = dirigido
+    def insertar_vertice(grafo,dato):
+        nodo = nodoVertice()
+        if grafo.inicio is None or dato < grafo.inicio.info:
+            nodo.sig = grafo.inicio
+            grafo.inicio = nodo
+        else:
+            ant = grafo.inicio
+            act = grafo.inicio.sig
+            while act is not None and act.info < nodo.info:
+                ant = act
+                act = act.sig
+            nodo.sig = act
+            ant.sig = nodo
+        grafo.tamanio += 1
+
+
+
+
+class Arista(object):
+    de
