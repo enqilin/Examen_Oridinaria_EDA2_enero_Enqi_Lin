1e6dd0c25378da700b8ecfeb79ab8be7750c75a8
nadous <114181724+enqilin@users.noreply.github.com>
2023-05-25 07:20:27 +0000

    cambio de 2 y 3

--- pregunta2.py
+++ pregunta2.py
@@ -11,6 +11,8 @@ los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velo
 Crea una lista con un numero arbitrario de objetos tipo Pokemon. Recorre los elementos de la lista, y prueba a ejecutar el método clasificacion de cada objeto que has creado.
 """
 
+import csv
+
 class Pokemon:
     def __ini__T(self,nombre,tipo):
         self.nombre = nombre
@@ -24,4 +26,24 @@ class Pokemon:
         self.Ataque__Especial = Ataque__Especial
         self.Defensa_Especial = Defensa_Especial
         self.Velocidad = Velocidad
+class pokemon:
+    lista = []
+    with open('pokemon.csv', newline = '\n') as fichero:
+        reader = csv.reader(fichero , delimiter= ';')
+        for nombre , tipo, PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad in reader:
+            pokemon = Pokemon(nombre , tipo, PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad)
+            lista.append(pokemon)
+    @staticmethod
 
+    def crear(nombre,tipo,PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad):
+        pokemon = Pokemon(nombre,tipo)
+        pokemon.clasificacion(Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad)
+        pokemon.lista.append(pokemon)
+        pokemon.guardar()
+        return pokemon
+
+    def guardar():
+        with open("pokemon.csv", 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero,delimiter = ';')
+            for pokemon in pokemon.lista:
+                writer.writerow(pokemon.nombre,pokemon.tipo,pokemon.PS,pokemon.Ataque,pokemon.Defensa,pokemon.Ataque__Especial,pokemon.Defensa_Especial,pokemon.Velocidad)
--- pregunta3.py
+++ pregunta3.py
@@ -12,10 +12,10 @@ por ejemplo, prueba a modificar el precio de una de las Pokeballs.
 """
 
 
+import csv
 
 class Pokemon:
-    lista = []
-    def __init__(self,nombre,tipo):
+    def __ini__T(self,nombre,tipo):
         self.nombre = nombre
         self.tipo = tipo
 
@@ -27,7 +27,24 @@ class Pokemon:
         self.Ataque__Especial = Ataque__Especial
         self.Defensa_Especial = Defensa_Especial
         self.Velocidad = Velocidad
+class pokemon:
+    lista = []
+    with open('pokemon.csv', newline = '\n') as fichero:
+        reader = csv.reader(fichero , delimiter= ';')
+        for nombre , tipo, PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad in reader:
+            pokemon = Pokemon(nombre , tipo, PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad)
+            lista.append(pokemon)
+    @staticmethod
 
-    def __str__(self):
-        return f."{self.nombre} es de tipo {self.tipo} ."
+    def crear(nombre,tipo,PS,Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad):
+        pokemon = Pokemon(nombre,tipo)
+        pokemon.clasificacion(Ataque,Defensa,Ataque__Especial,Defensa_Especial,Velocidad)
+        pokemon.lista.append(pokemon)
+        pokemon.guardar()
+        return pokemon
 
+    def guardar():
+        with open("pokemon.csv", 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero,delimiter = ';')
+            for pokemon in pokemon.lista:
+                writer.writerow(pokemon.nombre,pokemon.tipo,pokemon.PS,pokemon.Ataque,pokemon.Defensa,pokemon.Ataque__Especial,pokemon.Defensa_Especial,pokemon.Velocidad)
