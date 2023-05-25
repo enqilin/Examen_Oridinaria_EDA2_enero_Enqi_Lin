c2df41e65fa80f192d99362c17556a63ea24003e
nadous <114181724+enqilin@users.noreply.github.com>
2023-05-25 08:36:20 +0000

    test

--- test/test.py
+++ test/test.py
@@ -5,3 +5,8 @@ import pregunta2 as db
 class test(unnitest.TestCase):
     def setUp(self):
         db.pokemon.lista = [db.Pokemon("Ratata","normal"),
+                            db.Pokemon("Pikachu","electrico"),
+                            db.Pokemon("Charizard","fuego")]
+    
+    def test_clalificacion(self):
+        
